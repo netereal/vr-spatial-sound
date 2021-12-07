7a38e7aa8d17d76e5f818391d33771c568ff2487
Maria Novik <novik.maria@gmail.com>
2021-12-07 04:15:43 +0100

    collision trials

---
+++ Assets/01_demo_vr-hearing-loss-simulation/Scripts/CollisionAudioPlay.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionAudioPlay : MonoBehaviour
+{
+    public AudioSource sound;
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
+    void Update ()
+    {
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "floor")
+        {
+            sound.Play();
+            Destroy(collision.gameObject);
+        }
+        
+    }
+    
+}
+  
